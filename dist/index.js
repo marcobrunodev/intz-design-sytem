7f733789464edb13e8ec1ef834daf0ebb4c1d179
Marco Bruno <marco.bruno.br@gmail.com>
2021-02-18 16:10:37 +0100

    :bookmark: Version 0.1.4

--- dist/index.js
+++ dist/index.js
@@ -21,11 +21,19 @@ Object.defineProperty(exports, "Avatar", {
     return _Avatar.default;
   }
 });
+Object.defineProperty(exports, "Styles", {
+  enumerable: true,
+  get: function get() {
+    return _Styles.default;
+  }
+});
 
 var _Logo = _interopRequireDefault(require("./components/Logo"));
 
 var _Title = _interopRequireDefault(require("./components/Title"));
 
 var _Avatar = _interopRequireDefault(require("./components/Avatar"));
+
+var _Styles = _interopRequireDefault(require("./Styles"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
