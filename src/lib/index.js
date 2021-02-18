7cf79ef9466fae71a28d9eab2016a0885307b0fa
Marco Bruno <marco.bruno.br@gmail.com>
2021-02-18 16:02:50 +0100

    :sparkles: Add Styles in lib

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,6 @@
 import Logo from './components/Logo'
 import Title from './components/Title'
 import Avatar from './components/Avatar'
+import Styles from './Styles'
 
-export { Logo, Title, Avatar }
+export { Logo, Title, Avatar, Styles }
