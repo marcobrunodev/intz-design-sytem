b00405634202a9b6b70a3b3a2a2890da7528855a
Marco Bruno <marco.bruno.br@gmail.com>
2021-03-01 16:35:04 +0100

    :bookmark: 0.1.10

--- dist/Styles/index.js
+++ dist/Styles/index.js
@@ -9,14 +9,12 @@ var _react = _interopRequireDefault(require("react"));
 
 var _Settings = _interopRequireDefault(require("./Settings"));
 
-var _Generic = _interopRequireDefault(require("./Generic"));
-
 var _Elements = _interopRequireDefault(require("./Elements"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var Styles = function Styles() {
-  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement(_Settings.default, null), /*#__PURE__*/_react.default.createElement(_Generic.default, null), /*#__PURE__*/_react.default.createElement(_Elements.default, null));
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement(_Settings.default, null), /*#__PURE__*/_react.default.createElement(_Elements.default, null));
 };
 
 var _default = Styles;
